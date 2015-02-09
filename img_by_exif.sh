f73f583ef7ffedae3feff727cc28115902534678
esturniolo <esturniolo@gmail.com>
2015-02-09 20:29:35 -0300

    Creacion de backup con progreso

--- img_by_exif.sh
+++ img_by_exif.sh
@@ -1,42 +1,75 @@
 #!/bin/bash
 echo "Ingrese el path absoluto de imágenes a ordenar"
-read -e DirOri
+read -e DIRORI
+echo
+
 echo "Ingrese donde desea guardar las imágenes (se ordenaran en la ubicación elegida dentro de carpetas 'mes-año')"
-read -e DirDest
-echo "Las imagenes en ""$DirOri"" seran ordenadas en el directorio ""$DirDest""i"
+read -e DIRDEST
+echo
+
+echo "Las imagenes en ""$DIRORI"" seran ordenadas en el directorio ""$DIRDEST"""
+echo
+
+DATE=`date +%d-%m-%y_%H%M`
+TOTALFILES=`ls -l $DIRORI | wc -l`
+
+echo -n "Creando backup ..."; backup=`tar -cvzPf /tmp/Backup_Exif_$DATE.tgz $DIRORI` 2> ./error.log &
+PID=$!
+
+while kill -0 $PID 2> /dev/null; do
+echo -n "."
+sleep 2
+done
+
+echo -n " Backup finalizado."; 
+
+RC=`echo $?`
+
+if RC eq=0; then
+	0; else
+	echo "ERROR. Por favor revise el error en el archivo 'error.log'"
+fi
+	
+echo $RC
+
+
+echo "Cantidad total de imágenes a ordenar "$TOTALFILES""
+echo
+
+
 
 read -n1 -r -p "Presione Enter para continuar o CTRL - C para abortar operación"
 
 
 echo "-- Empieza script --"
-if [ -d $DirDest ]; then
+if [ -d $DIRDEST ]; then
         0; else
-        mkdir $DirDest
+        mkdir $DIRDEST
 fi;
 
 
-cd $DirOri
+cd $DIRORI
 
 echo "-- Empieza For --"
 
-for img in *;
-        do identify -format %[EXIF:DateTimeOriginal] $img &&
+for IMG in *;
+        do identify -format %[EXIF:DateTimeOriginal] $IMG &&
         echo "-- Paso fecha --"
-                anio=`echo ${img:0:4}` &&
-                mes=`echo ${img:4:4}` &&
-                tmp=`echo $mes""$anio` &&
-                fecha=`echo ${tmp:1:7}` &&
-                echo $fecha
+                ANIO=`echo ${IMG:0:4}` &&
+                MES=`echo ${IMG:4:4}` &&
+                TMP=`echo $MES""$ANIO` &&
+                FECHA=`echo ${TMP:1:7}` &&
+                echo $FECHA
         echo "-- Paso if --"
-                if [ -d $DirDest/$fecha ]; then
-                        mv $img $DirDest/$fecha; else
-                        garlopa=garlopa
+                if [ -d $DIRDEST/$FECHA ]; then
+                        mv $IMG $DIRDEST/$FECHA; else
+                        GARLOPA=garlopa
                 fi;
         echo "-- Comienza Case --"
-                case $garlopa in
+                case $GARLOPA in
                                 *)
                                         echo "Primer case"
-                                        mkdir $DirDest/$fecha && mv $img $DirDest/$fecha
+                                        mkdir $DIRDEST/$FECHA && mv $IMG $DIRDEST/$FECHA
                                         ;;
                 esac
         echo "-- Termina case --"
