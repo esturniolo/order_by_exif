315c8a994eb3721dd09d6a90d8604fd603c41379
Emilianogit <esturniolo@gmail.com>
2015-02-10 17:32:28 -0300

    falta la suma y verificacion de archivos para restore de backup

--- img_by_exif.sh
+++ img_by_exif.sh
@@ -6,25 +6,27 @@ backup=`tar -cvzPf /home/pi/ToshibaPi/Backup_Exif_$DATE.tgz $DIRORI`
 
 function EXIF(){
 for IMG in *;
-        do identify -format %[EXIF:DateTimeOriginal] $IMG &&
-        echo "-- Paso fecha --"
+        do identify -format %[EXIF:DateTimeOriginal] $IMG && 
+		
                 ANIO=`echo ${IMG:0:4}` &&
                 MES=`echo ${IMG:4:4}` &&
                 TMP=`echo $MES""$ANIO` &&
                 FECHA=`echo ${TMP:1:7}` &&
-                echo $FECHA
 
-        echo "-- Paso if --"
+
                 if [ -d $DIRDEST/$FECHA ]; then
 			mv $IMG $DIRDEST/$FECHA; else
 			mkdir $DIRDEST/$FECHA; mv $IMG $DIRDEST/$FECHA
                 fi
+
 done
+
 }
+
 
 function CREA_DEST(){
 if [ -d $DIRDEST ]; then
-	echo "THE NADA"; else
+	echo; else
 	echo -n "Creando carpeta "$DIRDEST""; mkdir $DIRDEST
 fi;
 }
@@ -36,6 +38,7 @@ if [ $RC -eq 0 ];then
 fi
 }
 
+DATE=`date +%d-%m-%y_%H%M`
 
 echo "Ingrese el path absoluto de imágenes a ordenar"
 read -e DIRORI
@@ -48,7 +51,6 @@ echo
 echo "Las imagenes en ""$DIRORI"" seran ordenadas en el directorio ""$DIRDEST"""
 echo
 
-DATE=`date +%d-%m-%y_%H%M`
 TOTALFILES=`ls -l $DIRORI | wc -l`
 
 echo
@@ -56,15 +58,12 @@ echo "Cantidad total de imágenes a ordenar: "$TOTALFILES""
 echo
 
 echo -n "Creando backup ..."; BKP > /dev/null 2> salida.log &
-PID=$!
+PIDBK=$!
 
-while kill -0 $PID 2> /dev/null; do
+while kill -0 $PIDBK 2> /dev/null; do
 echo -n "."
 sleep 2
 done
-
-echo
-echo $RC
 echo
 
 echo
@@ -74,10 +73,10 @@ echo
 read -n1 -r -p "Presione Enter para continuar o CTRL - C para abortar operación"
 
 
-echo "-- Empieza script --"
+#echo "-- Empieza script --"
 CREA_DEST
 
 cd $DIRORI
 
-echo "-- Empieza For --"
+#echo "-- Empieza For --"
 EXIF
